e9e5ed99a6eb7b91dcccbfabdf8cbf9e1b945a2b
LukyVj <lucas.bonomi@gmail.com>
2022-04-08 20:51:41 +0200

    update

--- src/scripts/snapshot.js
+++ src/scripts/snapshot.js
@@ -7,7 +7,9 @@ const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
   process.env.NEXT_PUBLIC_ALGOLIA_API_KEY
 );
-const index = searchClient.initIndex("algolia_canvas_place");
+const index = searchClient.initIndex(
+  process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME
+);
 
 let hits = [];
 
